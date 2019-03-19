ecaedd97b9a5e380e78148900e8ad7af995ffa99
ryoma <ryoma20151225@gmail.com>
2019-03-19 16:37:05 +0000

    First Commit.

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
-var express = require( 'express' );
-var http = require( 'http' );
+'use strict';
 
-var app            = express();
-app.set( 'port', process.env.PORT || 3001 );
-app.get('/', function (req, res) {
-    res.send('Hello World');
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+const defaultAccessToken = 'PS2uwMMzJqD1VhKeggCYoQXuS8N1XmQel8U5kfBnww1bh5VBGZvXFMTH0U08IBlMuwym/nFlKC1mG+OdE26JCPdpY0gvzY6UvhgVHxTusSSmfyMueu8KKhHTAWOG6lcPg5GnUMX4MKfdV24EARzt8QdB04t89/1O/w1cDnyilFU=';
+const defaultSecret = 'a624fb16c0828e450002e369a918e859';
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || defaultAccessToken,
+  channelSecret: process.env.CHANNEL_SECRET || defaultSecret,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-http.createServer( app ).listen( app.get( 'port' ), function (){
-  console.log( 'Express server listening on port ' + app.get( 'port' ));
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
